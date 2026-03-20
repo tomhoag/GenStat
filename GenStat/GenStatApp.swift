f7d2ac54d343a1da5a6cd3c6fd4b5a490cd3eecf
Tom Hoag <226088+tomhoag@users.noreply.github.com>
2026-03-20 16:58:22 -0400

    Initial Commit

---
+++ GenStat/GenStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenStatApp.swift
+//  GenStat
+//
+//  Created by Tom Hoag on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GenStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
